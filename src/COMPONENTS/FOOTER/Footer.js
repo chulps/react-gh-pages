6e64dd2b7864e0fa89f59f252fbb431be13818b6
Chuck Howard <choward@foundrydigital.com>
2022-11-01 17:47:53 -0700

    added a space in the footer after copyright symbol

--- src/COMPONENTS/FOOTER/Footer.js
+++ src/COMPONENTS/FOOTER/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
     return (
       <footer id="footer" className="bg-blueC flex-center flex-vertical p2">
         <img className="header-logo" src={NavLogo} alt="C-logo" />
-        ©Chuck Howard 2022
+        © Chuck Howard 2022
       </footer>
     );
   }
