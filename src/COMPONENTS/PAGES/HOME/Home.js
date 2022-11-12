4de4fb2529bdcdf53f9d9d38e21847851a6a73d6
Chuck Howard <choward@foundrydigital.com>
2022-11-11 20:37:32 -0800

    tweaking bio some more

--- src/COMPONENTS/PAGES/HOME/Home.js
+++ src/COMPONENTS/PAGES/HOME/Home.js
@@ -157,12 +157,13 @@ const Home = () => {
           <div>
             <label>Summary:</label>
             <h3 className="mb2">About me:</h3>
-            <p>Based in Los Angeles, I came here as an artist and designer in 2017. In 2018 I started coding and began my new career in tech, blending my skills and learning to make digital products.</p>
+            <p>Based in Los Angeles, I came here as an artist and designer in 2017. In 2018 I started coding and began my new career in tech, blending my old skills with more recent ones, and learning to make digital products.</p>
             <p>
               The road to where I am today was not a direct path. In my journey I've had interesting titles, and practiced different disciplines, but I always play the same role; <b>I am the one who designs the thing we sell.</b>
             </p>
+            <p>In doing this, I practice these core disciplines:</p>
             <p className="disciplines">
-              <small style={{ background: 'rgba(0, 143, 195, 0.25)' }}>UI/UX Design</small>
+              <small style={{background: 'rgba(0, 143, 195, 0.25)' }}>UI/UX Design</small>
               <small style={{background: 'rgba(255, 96, 96, 0.25)'}}>Front-end Development</small>
               <small style={{background: 'rgba(0, 206, 89, 0.25)'}}>Product Discovery</small>
             </p>
