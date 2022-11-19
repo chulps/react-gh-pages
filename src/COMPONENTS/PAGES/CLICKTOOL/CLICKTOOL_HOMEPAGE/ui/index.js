87baaf44b04603936b96d1cec8bfc4bf3e09a26d
Chuck Howard <choward@foundrydigital.com>
2022-11-19 14:36:49 -0800

    deleted clicktool homepage because its getting stuck

--- src/COMPONENTS/PAGES/CLICKTOOL/CLICKTOOL_HOMEPAGE/ui/index.js
+++
@@ -1,9 +0,0 @@
-import Button from './button/Button'
-import Chuckbox from './chuckbox/Chuckbox'
-import Dropdown from './dropdown/Dropdown'
-import Input from './input/Input'
-import Modal from './modal/Modal'
-import OptionGroup from './optionGroup/OptionGroup'
-import Refresh from './refreshButton/RefreshButton'
-
-export { Button, Chuckbox, Dropdown, Input, Modal, OptionGroup, Refresh }
