f11ed2e261b0620f4e5462fdaf5fd9a447a0e6bf
Chuck Howard <choward@foundrydigital.com>
2022-11-19 17:47:54 -0800

    had to restart my machine wtf

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Nucleotide from "./COMPONENTS/PAGES/NUCLEOTIDE/Nucleotide.js";
 import CRIGenetics from "./COMPONENTS/PAGES/CRIGENETICS/CRIGenetics.js";
 import Yarden from "./COMPONENTS/PAGES/YARDEN/Yarden.js";
 import Clicktool from "./COMPONENTS/PAGES/CLICKTOOL/Clicktool.js";
-import ClicktoolHomepage from "./COMPONENTS/PAGES/CLICKTOOL/CLICKTOOL_HOMEPAGE/Home.js"
 import Footer from "./COMPONENTS/FOOTER/Footer.js";
 import Covid from "./COMPONENTS/PAGES/COVID/Covid.js";
 import Foundry from "./COMPONENTS/PAGES/FOUNDRY/Foundry.js";
@@ -16,15 +15,7 @@ function App() {
   return (
     <div id="chuck" className="App nucleotide">
       <BrowserRouter forceRefresh={true}>
-        
-        <Routes>
-            <Route
-              path="/clicktool/clicktool_homepage/"
-              element={<ClicktoolHomepage />}
-            />
-        </Routes>
-
-        <body className="nucleotide">
+      
         <Header />
 
         {/* site content renders here */}
@@ -55,7 +46,6 @@ function App() {
         <ProjectSection />
 
           <Footer />
-          </body>
       </BrowserRouter>
     </div>
   );
