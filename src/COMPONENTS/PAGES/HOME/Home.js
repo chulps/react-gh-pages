7f7c5708fe4c23b77c8194e961b07dde7cb20db0
Chuck Howard <choward@foundrydigital.com>
2022-11-23 13:14:16 -0800

    removed that padding on md screens

--- src/COMPONENTS/PAGES/HOME/Home.js
+++ src/COMPONENTS/PAGES/HOME/Home.js
@@ -69,7 +69,7 @@ const Home = () => {
           </div>
           <div
 
-            className="flex-vertical flex-start md-flex-center-x pt2 pb4 lg-ml3"
+            className="flex-vertical flex-start md-flex-center-x pt2 pb4 md-pb-none lg-ml3"
           >
             <div
               className="content-box flex-vertical"
