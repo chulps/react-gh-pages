62f92e8a61c2e1b4f802d0cb3c36e338e8605f60
Chuck Howard <choward@foundrydigital.com>
2023-04-08 04:30:23 +0900

    unlocked cri genetics page

--- src/COMPONENTS/PAGES/CRIGENETICS/CRIGenetics.js
+++ src/COMPONENTS/PAGES/CRIGENETICS/CRIGenetics.js
@@ -4,7 +4,6 @@ import { Helmet } from "react-helmet";
 import Meowbert from "./Meowbert";
 import MeowbertCS from "./Meowbert_CS";
 
-
 class CRIGenetics extends Component {
   componentDidMount() {
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -86,7 +85,7 @@ class CRIGenetics extends Component {
               <div>
                 <h5>Design System</h5>
                 <small>
-                  A robust, token based library used to create a branded CSS
+                  <a href="/#/nucleotide" className="h6">Nucleotide</a>, A robust, token based library used to create a branded CSS
                   framework and React Component Library, in concert with an API
                   for use by the developers at CRI Genetics.
                 </small>
@@ -139,7 +138,9 @@ class CRIGenetics extends Component {
               <p>
                 In the first quarter of 2020 COVID was getting spicy. I was
                 working at <a href="/clicktool">Clicktool</a> and the company
-                pivoted from building a subscription based marketing analytics product to building a Shopify ecommerce site...
+                pivoted from building a subscription based marketing analytics
+                product to building a Shopify ecommerce site and designing goods
+                that I couldn't bring myself to agree with... so I quit.
               </p>
               <p>
                 Zip Recruiter has this feature, "1-Click-Apply" which I
@@ -161,67 +162,22 @@ class CRIGenetics extends Component {
         </section>
 
         <section>
-          <label>First task:</label>
+          <label>🛠️ Under construction:</label>
           <h2>
-            "Draw a Cat"{" "}
-            <span style={{ fontWeight: "normal", fontStyle: "italic" }}>
-              - The CEO
-            </span>
+            I'm still working on this page...
           </h2>
         </section>
         <section className="md-two-columns gap3 md-gap4 mt1">
-          <div>
-            <img
-              width="100%"
-              src="https://static.vecteezy.com/system/resources/previews/000/156/144/large_2x/female-and-male-customer-service-vector.jpg"
-              alt="Easy Cheesy Vecteezy"
-            />
-            <small>
-              The girl on the left can be found in a few places among CRI's products and websites... she might still be there 😅
-            </small>
-            <br />
-            <small>
-              Source:
-              https://static.vecteezy.com/system/resources/previews/000/156/144/large_2x/female-and-male-customer-service-vector.jpg
-            </small>
+          <MeowbertCS />
+
+          <div className="flex-center flex-vertical p3">
+            <p className="mb2">Click here to view a demo page of the site thats still active</p>
+            <a href="https://demo.crigenetics.com/dash" target="_blank"><button className="green">View Demo</button></a>
           </div>
 
-          <div>
-            <p>
-              My new boss wanted to give me a fun new project to welcome me to
-              the team. The designer who came before me (who I have much respect
-              for) was not a designer who codes but instead, a coder who
-              designs.
-            </p>
-            <p>
-              The illustrations in our app were taken from some royalty-free
-              images website and my boss wanted something with more personality
-              that would more easily help our users connect with the product.
-            </p>
-            <p>Also, our 404 page was looking really plain</p>
-          </div>
-
-          <div>
-            <div style={{ position: "sticky", top: "var(--unit4)" }}>
-              <label>re: "draw a cat"</label>
-              <h3>This is Meowbert Einstein</h3>
-
-              <p>
-                He only knows two things:
-                <br />
-                1. Science
-                <Meowbert />
-                <br />
-                2. Customer Service
-                <MeowbertCS />
-              </p>
-            </div>
-          </div>
-          <div>meowbert</div>
         </section>
-        <hr className="my3" />
-        <p className="text-center">More about CRI Genetics coming later...</p>
-
+        <hr className="mt3 mb3" />
+        <p className="text-center mb3">More about CRI Genetics coming later...</p>
       </div>
     );
   }
