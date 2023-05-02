627eb37aa3780520361e00a3d359e9e1d79e0003
Chuck Howard <choward@foundrydigital.com>
2023-05-02 15:56:00 +0900

    fixed the covid api backend

--- src/COMPONENTS/PAGES/COVID/api.js
+++ src/COMPONENTS/PAGES/COVID/api.js
@@ -1,22 +1,32 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
 
-dotenv.config();
+// const fetchCovidStats = async () => {
+//   try {
+//     const options = {
+//       method: 'GET',
+//       url: 'https://covid-193.p.rapidapi.com/statistics',
+//       headers: {
+//         'X-RapidAPI-Key': '9f5d0c8553msh7e4cfdabfc5e0f9p112f57jsnde15b5674f41',
+//         'X-RapidAPI-Host': 'covid-193.p.rapidapi.com',
+//       },
+//     };
+//     const response = await axios.request(options);
+//     return { data: response.data.response, error: null };
+//   } catch (err) {
+//     return { data: null, error: err.message };
+//   }
+// };
+
+
+// Import axios (you should have axios installed in your React project)
 
 const fetchCovidStats = async () => {
   try {
-    const options = {
-      method: 'GET',
-      url: 'https://covid-193.p.rapidapi.com/statistics',
-      headers: {
-        'X-RapidAPI-Key': '9f5d0c8553msh7e4cfdabfc5e0f9p112f57jsnde15b5674f41',
-        'X-RapidAPI-Host': 'covid-193.p.rapidapi.com',
-      },
-    };
-    const response = await axios.request(options);
-    return { data: response.data.response, error: null };
-  } catch (err) {
-    return { data: null, error: err.message };
+    const response = await axios.get('https://limitless-lake-38337.herokuapp.com/api/covid');
+    return response;
+  } catch (error) {
+    console.error('Error fetching COVID stats:', error.message);
+    return null;
   }
 };
 
