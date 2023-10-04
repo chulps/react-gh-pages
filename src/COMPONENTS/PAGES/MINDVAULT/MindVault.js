337d1924f8f38d681f4cddf8281414061ba2b478
Chuck Howard <chuckoward@gmail.com>
2023-10-04 14:19:33 +0900

    started building mindvault page

---
+++ src/COMPONENTS/PAGES/MINDVAULT/MindVault.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./MindVault.css";
+
+class MindVault extends React.Component {
+  render() {
+    return (
+      <div className="mindvault">
+
+              </div>
+)}};
