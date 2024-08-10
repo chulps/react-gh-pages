2c78e6eed35e8825b2a95d9c02bc89bc6a924d8e
Chuck Howard <chuckoward@gmail.com>
2024-08-10 16:43:17 +0900

    finished tchat

--- src/COMPONENTS/PAGES/TCHAT/TChat.js
+++ src/COMPONENTS/PAGES/TCHAT/TChat.js
@@ -16,6 +16,7 @@ import MVPSection from "./MVPSection";
 import ProjectOverviewSection from "./ProjectOverviewSection";
 import MVPFeaturesSection from "./MVPFeaturesSection";
 import TechnologyUsed from "./TechnologyUsed";
+import BuildingOnTheMVPSection from "./BuildingOnTheMVPSection";
 
 // Create a component for the competitor analysis table
 
@@ -27,7 +28,7 @@ const TChat = () => {
       </Helmet>
 
       <section className="pt3">
-        <label>Project:</label>
+        <label>Project case study:</label>
         <h1>T-Chat</h1>
       </section>
 
@@ -72,36 +73,8 @@ const TChat = () => {
 
       <TechnologyUsed />
 
-      <section className="lg-two-columns gap3 mt4">
-        <div className="prototype-frame">
-          <iframe
-            title="`T-Chat` Figma Prototype"
-            style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }}
-            width="100%"
-            height="650"
-            src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FINUShNpcGk0fqAYsZ1nUwo%2FT-Chat%3Fpage-id%3D0%253A1%26node-id%3D3121-427%26viewport%3D-3370%252C1333%252C0.58%26t%3D8UsjU9MZ0Mn1ym9l-1%26scaling%3Dscale-down%26content-scaling%3Dfixed%26starting-point-node-id%3D3121%253A427"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div>
-          <label>Building on the MVP</label>
-          <h3>Enhancing the Experience with Advanced Features</h3>
-          <p>
-            As T-Chat progresses beyond the initial MVP, the next phase of
-            development focuses on incorporating additional features that
-            enhance user experience and functionality. These enhancements
-            include Authentication, allowing users to securely access their
-            profiles and data, and the ability to save conversations for future
-            reference. In-app notifications will keep users updated in real
-            time, while the option to add friends or contacts expands the social
-            aspect of the platform. Customization options like adjustable font
-            sizes and toggling the display of original untranslated messages
-            will provide users with a more tailored experience. Additionally,
-            users will be able to search for contacts or chatrooms, making it
-            easier to stay connected and manage conversations effectively.
-          </p>
-        </div>
-      </section>
+      <BuildingOnTheMVPSection />
+
 
       <section className="mt4">
         <label>Taking the MVP Into the Wild</label>
@@ -128,7 +101,9 @@ const TChat = () => {
             meet user needs.
           </p>
 
-          <p>Positive feedback is good but... negative feeback has real value.</p>
+          <p>
+            Positive feedback is good but... negative feeback has real value.
+          </p>
         </div>
         <div className="sm-two-columns lg-one-column xl-two-columns">
           <div className="bg-green4 p2">
@@ -352,22 +327,25 @@ const TChat = () => {
             </p>
           </li>
         </ul>
-
       </section>
 
-<div className="bg-blue4 py4 mt4">
-        <section style={{maxWidth: 'var(--unit7)'}}>
+      <div className="bg-blue4 py4 mt4">
+        <section style={{ maxWidth: "var(--unit7)" }}>
           <h1>Final Conclusion</h1>
           <p>
             The T-Chat project showcases my ability to lead a design effort from
             concept to implementation. It highlights my skills in user research,
-            UI/UX design, and technical execution, aligning well with the goal of
-            enhancing communication through technology.
+            UI/UX design, and technical execution, aligning well with the goal
+            of enhancing communication through technology.
           </p>
-  
-          <div className="flex-center pt3"><button>Try T-Chat</button></div>
+
+          <div className="flex-center pt3">
+            <a href="https://chulps.github.io/chat-app/">
+              <button>Try T-Chat</button>
+            </a>
+          </div>
         </section>
-</div>
+      </div>
     </div>
   );
 };
